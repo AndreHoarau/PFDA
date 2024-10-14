2584256c2ed742ef149f670d999d787dc9c5e4ac
Andre Hoarau <G00439332@atu.ie>
2024-10-14 20:44:13 +0100

    getting my repository up to date

--- mywork/week1/lab6.py
+++ mywork/week1/lab6.py
@@ -1,7 +1,3 @@
 # Read JSON from internet
 # Author Andre Hoarau
-import requests
-url ="https://api.coindesk.com/v1/bpi/currentprice.json"
-response = requests.get(url)
-data = response.json()
-print (data)
+    
