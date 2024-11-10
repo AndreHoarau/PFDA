df2707e9049f9f388ca46eafd7bbb764e9fc336b
Andre Hoarau <G00439332@atu.ie>
2024-11-10 16:08:04 +0000

    working on the assignemnt

---
+++ mywork/week6_datetime/breakout1.py
@@ -0,0 +1,8 @@
+# Author Andre Hoarau
+# Following jupyter notebook 1
+#Breakout 1
+# Create a time object with 5 microseconds and 39 minutes.
+import datetime
+
+t = datetime.time(minute= 39, microsecond= 5)
+print(t)
